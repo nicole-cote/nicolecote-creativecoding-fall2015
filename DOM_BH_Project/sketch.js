6a13e2679451f158b92f40a48ff0b8aa49541e4c
nicole-cote <nicole.cote@nyu.edu>
2015-10-21 14:37:15 -0400

    adds DOM BH project

---
+++ DOM_BH_Project/sketch.js
@@ -0,0 +1,266 @@
+var imgBackground 
+
+var BH1;
+var BH2;
+var BH3;
+var BH4;
+var BH5;
+var BH6;
+var BH7;
+var BH8;
+var BH9;
+var BH10;
+var BH11;
+var BH12;
+var BH13;
+var BH14;
+var BH15;
+var BH16;
+var BH17;
+var BH18;
+
+function setup() {
+  //sets up bacgkround image  
+  imgBackground = createImg("https://londonbygaslight.files.wordpress.com/2013/08/fog.jpg");
+  imgBackground.position (0,0);
+  imgBackground.size (windowWidth, windowHeight);
+}
+
+function draw() {
+  BH1 = createDiv("London. Michaelmas term lately over.... Implacable November weather. Mud in the streets....");
+  BH1.position(5, 5);
+  BH1.style("color", "#908D8D");
+  BH1.style("font-family", "Times New Roman");
+  BH1.style("font-size", "14pt");
+  BH1.style("opacity", ".05");
+
+  
+  BH2 = createDiv("Smoke lowering down from chimney-pots, making a soft black drizzle, with flakes of soot....");
+  BH2.position(5, 25);
+  BH2.style("color", "#908D8D");
+  BH2.style("font-family", "Times New Roman");
+  BH2.style("font-size", "14pt");
+  BH2.style("opacity", ".05");
+
+  
+  BH3 = createDiv("Dogs, undistinguishable in mire. Horses, scarcely better; splashed to their very blinkers....");
+  BH3.position(5, 45);
+  BH3.style("color", "#908D8D");
+  BH3.style("font-family", "Times New Roman");
+  BH3.style("font-size", "14pt"); 
+  BH3.style("opacity", ".05");
+
+  
+  BH4 = createDiv("Foot passengers, jostling one another's umbrellas in a general infection of ill temper....");
+  BH4.position(5, 65);
+  BH4.style("color", "#908D8D");
+  BH4.style("font-family", "Times New Roman");
+  BH4.style("font-size", "14pt"); 
+  BH4.style("opacity", ".05");
+
+  
+  BH5 = createDiv("Foot passengers have been slipping and sliding since the day broke (if this day ever broke)....");
+  BH5.position(5, 85);
+  BH5.style("color", "#908D8D");
+  BH5.style("font-family", "Times New Roman");
+  BH5.style("font-size", "14pt"); 
+  BH5.style("opacity", ".05");
+
+  
+  BH6 = createDiv("Adding new deposits to the crust upon crust of mud...");
+  BH6.position(5, 105);
+  BH6.style("color", "#908D8D");
+  BH6.style("font-family", "Times New Roman");
+  BH6.style("font-size", "14pt"); 
+  BH6.style("opacity", ".05");
+
+
+  BH7 = createDiv("Sticking at those points tenaciously to the pavement, and accumulating at compound interest.");
+  BH7.position(5, 125);
+  BH7.style("color", "#908D8D");
+  BH7.style("font-family", "Times New Roman");
+  BH7.style("font-size", "14pt"); 
+  BH7.style("opacity", ".05");
+
+  
+  BH8 = createDiv("Fog everywhere.");
+  BH8.position(5, 145);
+  BH8.style("color", "#908D8D");
+  BH8.style("font-family", "Times New Roman");
+  BH8.style("font-size", "14pt"); 
+  BH8.style("opacity", ".05");
+
+  
+  BH9 = createDiv("Fog up the river, where it flows among green aits and meadows;");
+  BH9.position(5, 165);
+  BH9.style("color", "#908D8D");
+  BH9.style("font-family", "Times New Roman");
+  BH9.style("font-size", "14pt"); 
+  BH9.style("opacity", ".05");
+
+  
+  BH10 = createDiv("Fog down the river, where it rolls defiled among the tiers of shipping and the waterside pollutions of a great (and dirty) city.");
+  BH10.position(5, 185);
+  BH10.style("color", "#908D8D");
+  BH10.style("font-family", "Times New Roman");
+  BH10.style("font-size", "14pt"); 
+  BH10.style("opacity", ".04");
+
+  
+  BH11 = createDiv("Fog on the Essex marshes.");
+  BH11.position(5, 205);
+  BH11.style("color", "#908D8D");
+  BH11.style("font-family", "Times New Roman");
+  BH11.style("font-size", "14pt"); 
+  BH11.style("opacity", ".04");
+
+  
+  BH12 = createDiv("Fog on the Kentish heights.");
+  BH12.position(5, 225);
+  BH12.style("color", "#908D8D");
+  BH12.style("font-family", "Times New Roman");
+  BH12.style("font-size", "14pt"); 
+  BH12.style("opacity", ".04");
+
+  
+  BH13 = createDiv("Fog creeping into the cabooses of collier-brigs.");
+  BH13.position(5, 245);
+  BH13.style("color", "#908D8D");
+  BH13.style("font-family", "Times New Roman");
+  BH13.style("font-size", "14pt"); 
+  BH13.style("opacity", ".04");
+
+  
+  BH14 = createDiv("Fog lying out on the yards and hovering in the rigging of great ships.");
+  BH14.position(5, 265);
+  BH14.style("color", "#908D8D");
+  BH14.style("font-family", "Times New Roman");
+  BH14.style("font-size", "14pt"); 
+  BH14.style("opacity", ".04");
+
+  
+  BH15 = createDiv("Fog drooping on the gunwales of barges and small boats.");
+  BH15.position(5, 285);
+  BH15.style("color", "#908D8D");
+  BH15.style("font-family", "Times New Roman");
+  BH15.style("font-size", "14pt"); 
+  BH15.style("opacity", ".04");
+
+  
+  BH16 = createDiv("Fog in the eyes and throats of ancient Greenwich pensioners, wheezing by the firesides of their wards....");
+  BH16.position(5, 305);
+  BH16.style("color", "#908D8D");
+  BH16.style("font-family", "Times New Roman");
+  BH16.style("font-size", "14pt"); 
+  BH16.style("opacity", ".04");
+
+  
+  BH17 = createDiv("Fog in the stem and bowl of the afternoon pipe of the wrathful skipper, down in his close cabin...");
+  BH17.position(5, 325);
+  BH17.style("color", "#908D8D");
+  BH17.style("font-family", "Times New Roman");
+  BH17.style("font-size", "14pt"); 
+  BH17.style("opacity", ".04");
+
+  
+  BH18 = createDiv("Fog cruelly pinching the toes and fingers of his shivering little 'prentice boy on deck.");
+  BH18.position(5, 345);
+  BH18.style("color", "#908D8D");
+  BH18.style("font-family", "Times New Roman");
+  BH18.style("font-size", "14pt");
+  BH18.style("opacity", ".035");
+
+
+  BH19 = createDiv("Chance people on the bridges peeping over the parapets into a nether sky of fog,");
+  BH19.position(5, 365);
+  BH19.style("color", "#908D8D");
+  BH19.style("font-family", "Times New Roman");
+  BH19.style("font-size", "14pt");
+  BH19.style("opacity", ".035");
+  
+  BH20 = createDiv("With fog all round them, as if they were up in a balloon and hanging in the misty clouds.");
+  BH20.position(5, 385);
+  BH20.style("color", "#908D8D");
+  BH20.style("font-family", "Times New Roman");
+  BH20.style("font-size", "14pt");
+  BH20.style("opacity", ".035");
+  
+  
+  BH21 = createDiv("Gas looming through the fog in divers places in the streets, much as the sun may, from the spongey fields,");
+  BH21.position(5, 405);
+  BH21.style("color", "#908D8D");
+  BH21.style("font-family", "Times New Roman");
+  BH21.style("font-size", "14pt");
+  BH21.style("opacity", ".035");
+  
+  
+  BH22 = createDiv("Be seen to loom by husbandman and ploughboy. Most of thee shops lightes two hours before their time--");
+  BH22.position(5, 425);
+  BH22.style("color", "#908D8D");
+  BH22.style("font-family", "Times New Roman");
+  BH22.style("font-size", "14pt");
+  BH22.style("opacity", ".035");
+  
+  
+  BH23 = createDiv("As the gas seems to know, for it has a haggard and unwilling look.");
+  BH23.position(5, 445);
+  BH23.style("color", "#908D8D");
+  BH23.style("font-family", "Times New Roman");
+  BH23.style("font-size", "14pt");
+  BH23.style("opacity", ".03");
+  
+  
+  BH24 = createDiv("The raw afternoon is rawest, and the dense fog is densest, and the muddy streets are muddiest");
+  BH24.position(5, 465);
+  BH24.style("color", "#908D8D");
+  BH24.style("font-family", "Times New Roman");
+  BH24.style("font-size", "14pt");
+  BH24.style("opacity", ".03");
+  
+  
+  BH25 = createDiv("Near that leaden-deaded old obstruction, appropriate ornament for the thresholdof a leaden-headed old corporation,");
+  BH25.position(5, 485);
+  BH25.style("color", "#908D8D");
+  BH25.style("font-family", "Times New Roman");
+  BH25.style("font-size", "14pt");
+  BH25.style("opacity", ".03");
+  
+  BH26 = createDiv("Temple Bar. And hard by Temple Bar, in Lincoln's Inn Hall, at the very heart of the fog,");
+  BH26.position(5, 505);
+  BH26.style("color", "#908D8D");
+  BH26.style("font-family", "Times New Roman");
+  BH26.style("font-size", "14pt");
+  BH26.style("opacity", ".03");
+  
+  
+  BH27 = createDiv("Sits the Lord High Chancellor in his High Court of Chancery.");
+  BH27.position(5, 525);
+  BH27.style("color", "#908D8D");
+  BH27.style("font-family", "Times New Roman");
+  BH27.style("font-size", "14pt");
+  BH27.style("opacity", ".03");
+  
+  
+  BH28 = createDiv("Never can there come fog too thick, never can there come mud and mire too deep,");
+  BH28.position(5, 545);
+  BH28.style("color", "#908D8D");
+  BH28.style("font-family", "Times New Roman");
+  BH28.style("font-size", "14pt");
+  BH28.style("opacity", ".03");
+  
+  
+  BH29 = createDiv("To assort with the groping and floundering condition which this High Court of Chancery,");
+  BH29.position(5, 565);
+  BH29.style("color", "#908D8D");
+  BH29.style("font-family", "Times New Roman");
+  BH29.style("font-size", "14pt");
+  BH29.style("opacity", ".03");
+  
+  
+  BH30 = createDiv("Most pestilent of hoary sinners, holds this day in the sight of heaven and earth.");
+  BH30.position(5, 585);
+  BH30.style("color", "#908D8D");
+  BH30.style("font-family", "Times New Roman");
+  BH30.style("font-size", "14pt");
+  BH30.style("opacity", ".03");
+}
